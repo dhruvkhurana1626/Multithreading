0e51880540145514e87993c96ec71eee11ea56af
Dhruv khurana <Dhruvkhurana162@gmail.com>
2026-08-19 17:00:33 +0530

    Practising Thread Creation

--- src/main/java/com/example/multithreading/CallableTester.java
+++ src/main/java/com/example/multithreading/CallableTester.java
@@ -6,16 +6,22 @@ public class CallableTester {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
+        //task
         Callable<Integer> task = () -> {
             return 10 + 20;
         };
 
+        //rather than creating threads manually
+        //im using executor service to create threads for me
         ExecutorService executor = Executors.newSingleThreadExecutor();
 
+        //future represent the future integer result from this task
         Future<Integer> result = executor.submit(task);
 
+        //wait for and retrieve the result
         System.out.println(result.get());
 
+        //shutdown the executor when finished
         executor.shutdown();
     }
 
